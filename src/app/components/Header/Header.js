db3d12791176690f57dddb0abbdfcf1eb1d54171
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-07 10:36:54 +0500

    make header logo as a link

--- src/app/components/Header/Header.js
+++ src/app/components/Header/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Header = () => {
   return (
     <div className='bg-white flex flex-col md:flex-row items-center justify-between py-5 px-5 lg:px-20'>
-        <img src="/images/logo.png" alt="" className='w-[250px]  object-contain' />
+        <a href="/"><img src="/images/logo.png" alt="" className='w-[250px]  object-contain' /></a>
         <img src="/images/trusted-form.png" alt="" className='w-[250px] object-contain' />
     </div>
   )
