c1588c33afb5df6053ac333df54cfc9841491dde
DESKTOP-TM8FG9P\Ali Ahmed <alii.amadd@gmail.com>
2023-09-14 13:38:26 +0500

    add facebook id

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,9 +1,11 @@
+import Script from 'next/script';
 import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import './globals.css'
 
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import FacebookPixel from './components/FacebookPixel';
 
 export const metadata = {
   title: 'Solar Energy',
@@ -13,7 +15,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      
       <body className="bg-[#235c94]">
+        <FacebookPixel />
         <ToastContainer />
         <Header />
         {children}
