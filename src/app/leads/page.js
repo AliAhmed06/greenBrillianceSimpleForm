f68e938de420408527882daadf9ba6ec15260584
Your Name~ <you@example.com>
2023-11-13 19:40:14 +0500

    api change 4

--- src/app/leads/page.js
+++ src/app/leads/page.js
@@ -29,12 +29,9 @@ const Page = () => {
 
   useEffect(() => {
     const getData = async () => {
-      let response = await fetch(
-        `${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/simpleForm`,
-        {
-          cache: "no-store",
-        }
-      );
+      let response = await fetch(`/api/simpleForm`, {
+        cache: "no-store",
+      });
       // let response = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN_NAME}/api/simpleForm`, { cache: 'no-store' });
       const contentType = response.headers.get("content-type");
       response = await response.json();
